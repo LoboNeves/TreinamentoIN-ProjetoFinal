3e90383a939fbd2aea467665594dbc62ff0398df
DanielNeves <dbenditoneves@gmail.com>
2020-08-25 19:12:45 -0300

    Adicionado scaffold payment

---
+++ db/migrate/20200825220929_create_payments.rb
@@ -0,0 +1,14 @@
+class CreatePayments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :payments do |t|
+      t.total_price :
+      t.string :address
+      t.string :payment_information
+      t.string :summary
+      t.string :calculation
+      t.string :total_price
+
+      t.timestamps
+    end
+  end
+end
