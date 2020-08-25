a5b1cd39e7fd8e3d9b8d6fd2dfbbf98aac0aaa79
DanielNeves <dbenditoneves@gmail.com>
2020-08-25 15:51:04 -0300

    Ainda tentando fazer a rota para users funcionar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   get '/', to: 'static_page#home'
-  get '/users', to: 'users#index' 
+  get 'https://projetofinalin.herokuapp.com/users', to: 'users#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
