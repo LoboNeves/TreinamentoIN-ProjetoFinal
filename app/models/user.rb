17efe42bdc63850c1720370525cd46814b1079a6
DanielNeves <dbenditoneves@gmail.com>
2020-08-25 15:45:22 -0300

    COnfigurado user model e colocado a rota dele em routes.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates :name, :lastname, presence: true
-    validates :email, uniqueness: true
+    has_secure_password #password_digest
+    validates :name, :lastname, :password, :password_confirmation, presence: true
+    validates :email, presence: true, uniqueness: true
+    #Diferenciação dos tipos de usuário
+    enum kind: {
+        admin: 0,
+        client: 1,
+        guest: 2
+    }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  get '/',to: 'static_page#home'
+  get '/', to: 'static_page#home'
+  get '/', to: 'users#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
