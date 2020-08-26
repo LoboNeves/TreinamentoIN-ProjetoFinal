83314674001f6ae82a3fd8e899816aa7ac719490
DanielNeves <dbenditoneves@gmail.com>
2020-08-26 15:40:12 -0300

    Teste login

--- db/migrate/20200825182136_create_users.rb
+++ db/migrate/20200825182136_create_users.rb
@@ -2,11 +2,15 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.string :lastname
+      #t.string :lastname
       t.string :email
       t.string :password_digest
       t.string :cep
+      t.string :address
       t.string :residencial_number
+      t.string :city
+      t.string :neighborhood
+      t.string :uf
       t.string :phone_number
       t.string :kind
 
