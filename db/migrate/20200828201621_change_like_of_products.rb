2bc28e83f014642b5ec73eeecd5b4a385bf85a37
DanielNeves <dbenditoneves@gmail.com>
2020-08-28 17:35:21 -0300

    Agora vai

--- db/migrate/20200828201621_change_like_of_products.rb
+++ db/migrate/20200828201621_change_like_of_products.rb
@@ -1,5 +1,5 @@
 class ChangeLikeOfProducts < ActiveRecord::Migration[6.0]
   def change
-    change_column :products, :like, :integer, using: "'like'::integer"
+    change_column :products, :like, :integer, "USING "like"::integer"
   end
 end
