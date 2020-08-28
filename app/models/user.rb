4707cbe265f1c1f23ce48384c62577bc83cc5563
DanielNeves <dbenditoneves@gmail.com>
2020-08-28 11:55:28 -0300

    Problema com migrations resolvido

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
     validates :name, :password, :password_confirmation, presence: true
     VALID_EMAIL_FORMAT= /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i #Validação de formato de email
     validates :email, presence: true, format: { with: VALID_EMAIL_FORMAT}, uniqueness: true
-    #Diferenciação dos tipos de usuário
 end
