f469fcaf452e9c77a270f94cf3daceacd88d14cd
DanielNeves <dbenditoneves@gmail.com>
2020-08-28 11:33:19 -0300

    Reverse de novo

--- app/models/payment.rb
+++
@@ -1,3 +0,0 @@
-class Payment < ApplicationRecord
-    validates :total_price, :address, :payment_information, :summary, :calculation, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :payments
   resources :carts
   resources :products
   resources :users 
