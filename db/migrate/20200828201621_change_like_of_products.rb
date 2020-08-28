e0e14f4cc9ecde437ed014f440060330a75c34a4
DanielNeves <dbenditoneves@gmail.com>
2020-08-28 17:32:15 -0300

    Tentando concertar erro do like

--- db/migrate/20200828201621_change_like_of_products.rb
+++ db/migrate/20200828201621_change_like_of_products.rb
@@ -1,5 +1,5 @@
 class ChangeLikeOfProducts < ActiveRecord::Migration[6.0]
   def change
-    change_column :products, :like, :integer
+    change_column :products, :like, 'integer USING CAST(like AS integer)'
   end
 end
