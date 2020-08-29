823fdb63499b0985bc227127ff763afad1e42dc4
DanielNeves <dbenditoneves@gmail.com>
2020-08-29 09:01:14 -0300

    Tentando mudar like para integer

--- db/migrate/20200828201621_change_like_of_products.rb
+++ db/migrate/20200828201621_change_like_of_products.rb
@@ -1,5 +1,5 @@
 class ChangeLikeOfProducts < ActiveRecord::Migration[6.0]
   def change
-    change_column :products, :like, :integer, using: 'like::integer', default: 0, null: false
+    change_column :products, :like, :integer
   end
 end
